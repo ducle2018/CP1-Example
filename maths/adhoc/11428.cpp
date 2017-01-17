8343d9d7c99e31cf43f75d9e7b8c6274dff28f24
Pham Viet An <phvietan@gmail.com>
2017-01-17 10:50:51 +0700

    Submitted by VietAn

---
+++ maths/adhoc/11428.cpp
@@ -0,0 +1,82 @@
+///Author: Pham Viet An
+///Topics: Mathematics - Ad hoc Problems
+///Problem: 8 - UVA11428 - Cubes
+///Language: C++
+
+/*<--------------------------------------------------------------------------------------->*/
+
+///x^3 - y^3 = n
+///(x-y) * (x^2 + xy + y^2) = n
+///x and y are integers and x > 0 and y > 0
+///=> x-y and x^2 + xy + y^2 are integers
+///=> x-y and x^2 + xy + y^2 are divisor of n
+///Lets call x-y = a; and x^2 + xy + y^2 = b; (a <= b)
+///Because x = a+y. Substitute it into x^2 + xy + y^2 = b
+///It becomes: 3y^2 + 3ay + a^2 = b
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<int,int> ii;
+
+vector <ii> res;
+
+bool isPerfectSquare(int n) {
+    int k = sqrt(n);
+    return k*k == n;
+}
+
+vector <int> squareEquation(int a, int b, int c) { ///solve Square Equation
+    vector <int> res;
+    int delta = b*b - 4*a*c;
+    if (delta < 0 || !isPerfectSquare(delta)) return res;
+
+    delta = sqrt(delta);
+    if ((-b - delta) % (2*a) == 0) res.push_back((-b - delta) / (2*a));
+    if ((-b + delta) % (2*a) == 0 && delta != 0) res.push_back((-b + delta) / (2*a));
+
+    return res;
+}
+
+void updateEquation(int a, int b) {
+    vector <int> solution = squareEquation(3,3*a,a*a-b);
+
+    for (int i = 0; i < (int)solution.size(); ++i) {
+        int y = solution[i];
+        int x = a+y;
+        if (x > 0 && y > 0)
+            res.push_back(ii(x,y));
+    }
+}
+
+void solve(int n) {
+    res.clear(); ///reset the result
+    int k = sqrt(n); ///iterate through all divisors of n
+    for (int i = 1; i <= k; ++i) {
+        if (n%i!=0) continue;
+        int a = i, b = n/i;
+        updateEquation(a,b);
+    }
+    if (res.size()==0) cout << "No solution" << endl;
+    else {
+        int minY = res[0].second; ///get minimum y among all results
+        for (int i = 0; i < (int)res.size(); ++i)
+            minY = min(minY,res[i].second);
+        for (int i = 0; i < (int)res.size(); ++i)
+            if (minY==res[i].second) {
+                cout << res[i].first << " " << res[i].second << endl;
+                break;
+            }
+    }
+}
+
+int main() {
+    for (;;) {
+        int n;
+        cin >> n;
+        if (n==0) break;
+        solve(n);
+    }
+	return 0;
+}
